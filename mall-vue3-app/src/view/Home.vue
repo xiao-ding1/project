b8ed29100ff4b1bc3acd3bde61b1bb0ea014674d
Wenjie Ding <wenjie.ding@shopee.com>
2025-12-02 23:15:15 +0800

    feat: 商品列表

--- mall-vue3-app/src/view/Home.vue
+++ mall-vue3-app/src/view/Home.vue
@@ -4,7 +4,7 @@
     <header class="home-header wrap" :class="{'active' : state.headerScroll}">
       <router-link tag="i" to="./category"><i class="nbicon nbmenu2"></i></router-link>
       <div class="header-search">
-        <span class="app-name">新蜂商城</span>
+        <span class="app-name"> 商城</span>
         <i class="iconfont icon-search"></i>
         <router-link tag="span" class="search-title" to="./product-list?from=home">山河无恙，人间皆安</router-link>
       </div>
@@ -86,11 +86,11 @@ const state = reactive({
   recommends: [],
   categoryList: [
     {
-      name: '新蜂超市',
+      name: ' 超市',
       imgUrl: 'https://s.yezgea02.com/1604041127880/%E8%B6%85%E5%B8%82%402x.png',
       categoryId: 100001
     }, {
-      name: '新蜂服饰',
+      name: ' 服饰',
       imgUrl: 'https://s.yezgea02.com/1604041127880/%E6%9C%8D%E9%A5%B0%402x.png',
       categoryId: 100003
     }, {
@@ -98,11 +98,11 @@ const state = reactive({
       imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%85%A8%E7%90%83%E8%B4%AD%402x.png',
       categoryId: 100002
     }, {
-      name: '新蜂生鲜',
+      name: ' 生鲜',
       imgUrl: 'https://s.yezgea02.com/1604041127880/%E7%94%9F%E9%B2%9C%402x.png',
       categoryId: 100004
     }, {
-      name: '新蜂到家',
+      name: ' 到家',
       imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%88%B0%E5%AE%B6%402x.png',
       categoryId: 100005
     }, {
